38bf5390fb3f026f743a5b384b576b1cbcfb0f99
Chirag-creator17 <76050897+Chirag-creator17@users.noreply.github.com>
2021-05-05 14:23:36 +0530

    Add files via upload

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+const Footer=()=>{
+    return (
+        <div className="footer">
+            <p>Made by: Chirag Goel</p>
+            <p>Copyright © {new Date().getFullYear()}</p>
+        </div>
+        )
+}
+export default Footer;
